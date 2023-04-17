133a9f571822a01dd47e8bcd56f137a157b700e8
Mehedi Hossain Hossain <>
2023-04-17 07:27:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+print(b)
+
+vb = '5'
+print("ff " + vb)
